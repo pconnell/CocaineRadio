27cb1cb2171fb7bb4dce2058da255936db32a17e
Patrick <pconnell89@gmail.com>
2011-09-05 19:19:34 -0400

    new embedded player!

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <!--AJAX for playlist updates-->
 </head>
 <body>
-<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=3116409&play_status=1' quality='high' 
+<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=
+http://yp.shoutcast.com/sbin/tunein-station.pls?id=3116409&play_status=1' quality='high' 
 bgcolor='#ffffff' width='398' height='104' name='popupPlayer_V19' align='middle' allowScriptAccess='always' allowFullScreen='true' 
 type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer' ></embed>
 <p><a href = "http://72.205.2.22:8000/listen.pls?sid=1">Download podcast here</a>
@@ -34,7 +35,7 @@ type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getfla
 			}
 		}
 	}
-	setInterval(update,5000);
+	setInterval(update,15000);
 </script>
 </body>
 </html>
