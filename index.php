f684d53e23046a03d6fdd01ef49806d27aba4e18
Patrick <pconnell89@gmail.com>
2011-09-08 16:00:47 -0400

    added tables to the site, and some links

--- index.php
+++ index.php
@@ -8,11 +8,46 @@
 <body>
 <br/>
 <center>
-<p id = "player"></p>
-<p><a href = "http://72.205.2.22:8000/listen.pls?sid=1">Download podcast here</a></p>
+<table id = 'projectData'>
+	<thead>
+		<tr>
+			<th width = "25%"><a href = "/about.php" target = '_blank'>Info/About</a></th>
+			<th width = "25%"><a href = "http://pconnell89.umwblogs.org" target = '_blank'>Blog</a></th>
+			<th width = "25%"><a href = "http://www.twitter.com/CocaineRadio" target = '_blank'>Twitter</a></th>
+			<th width = "25%"><a href = "http://www.facebook.com/patrick.s.connelly" target = '_blank'>Facebook</a></th>
+			<th><a></a></th>
+		</tr>
+	</thead>
+</table>
+<p id = "player"></p></tr>
+<p><a href = "http://72.205.2.22:8000/listen.pls?sid=1">Download podcast here (less jumpy)</a></p>
 </center>
 <!--AJAX for player/playlist updates-->
-<p id = 'playlist' name = 'playlist'></p>  
+<table border = '2' id = 'siteData'>
+<thead>
+	<tr>
+		<th width = "33%">Track History</th>
+		<th width = "33%">Vote!!</th>
+		<th width = "33%">Upload!!</th>
+	</tr>
+</thead>
+
+<tbody>
+	<tr>
+		<td><p id = 'playlist'></p></td>
+		<td></td>
+		<td></td>
+	</tr>
+</tbody>
+
+<tfoot>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+</tfoot>
+</table>
 <script type="text/javascript"> 
 	function update(){
 		var xmlhttp;
