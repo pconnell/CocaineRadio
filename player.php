6aa8844b3ecf5e2e1fbf7053591124e56e2b6f3a
Patrick <pconnell89@gmail.com>
2011-09-08 23:36:27 -0400

    added regex to smooth out the page

--- player.php
+++ player.php
@@ -6,14 +6,13 @@
 	$id = $match[0];
 	$str = '';
 	if ($id != ''){
-		$match = array();
-		$pattern = '/"[0-9]*/';
-		preg_match($pattern,$id,&$match);
-		$id = $match [0];
+		$patt = '/"[0-9]*/';
+		$mat = array();
+		preg_match($patt,$id,&$mat);
+		$id = $mat[0];
 		$id = substr($id,1);
 		//echo $id
 		$str = "<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=".$id."&play_status=1' quality='high' bgcolor='#ffffff' width='600' height='104' name='popupPlayer_V19' align='middle' allowScriptAccess='always' allowFullScreen='true' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer' ></embed>" ;
-
 	}else{
 		$str = "<em>We're sorry, but the stream is currently unavailable.</em>";
 	}
