cc37546115273af004140184e3750d68cab65dc9
Patrick <pconnell89@gmail.com>
2011-09-08 23:26:44 -0400

    added regex to smooth out the page

--- player.php
+++ player.php
@@ -1,14 +1,21 @@
 <?php
-	$html = implode('', file('http://www.shoutcast.com/Internet-Radio/CocaineRadio'));
-	$loc = strrpos($html,'a href="http://yp.shoutcast.com/sbin/tunein-station.pls?id=');
-	$loc2 = strrpos($html, '" title="CocaineRadio"');
-	$loc = $loc+59;
-	$len = $loc2-$loc;
-	$playerLoc = substr($html,$loc,$len);
-	$loc = strrpos($playerLoc,'" title="');
-	$playerLoc = substr($playerLoc,0,$loc);
-	//echo $playerLoc;
-	
-	$str = "<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=".$playerLoc."&play_status=1' quality='high' bgcolor='#ffffff' width='600' height='104' name='popupPlayer_V19' align='middle' allowScriptAccess='always' allowFullScreen='true' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer' ></embed>" ;
+	$html = file_get_contents('http://www.shoutcast.com/Internet-Radio/CocaineRadio');
+	$pattern = '/id="[0-9]*" title="CocaineRadio"/';
+	$match = array();
+	preg_match($pattern,$html,&$match);
+	$id = $match[0];
+	$str = '';
+	if ($id != ''){
+		$match = array();
+		$pattern = '/"[0-9]*/';
+		preg_match($pattern,$id,&$match);
+		$id = $match [0];
+		$id = substr($id,1);
+		//echo $id
+		$str = "<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=".$id."&play_status=1' quality='high' bgcolor='#ffffff' width='600' height='104' name='popupPlayer_V19' align='middle' allowScriptAccess='always' allowFullScreen='true' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer' ></embed>" ;
+
+	}else{
+		$str = "<em>We're sorry, but the stream is currently unavailable.</em>";
+	}
 	echo $str;
 ?>
