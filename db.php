a20d9ac945275d2f7bbdbfcbd4691b9501c3ac42
Patrick <pconnell89@gmail.com>
2011-09-18 19:30:04 -0400

    fixing db.php again...

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-	$db = mysql_real_connect('localhost:/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye') or die('Failed to connect');
+	$db = mysqli_connect('localhost:/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye') or die('Failed to connect');
 ?>
