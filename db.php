fa036daad7b5f135febac384fd54d347babcf4f5
Patrick <pconnell89@gmail.com>
2011-09-18 19:34:22 -0400

    fixed db.php IAW pagoda again...

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-	$db = new mysqli('/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye') or die('Failed to connect');
+	$db = new mysqli('/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye');// or die('Failed to connect');
 ?>
