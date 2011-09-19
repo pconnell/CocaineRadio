38e1c063a78f5af686ac5cb12bc5cf2ec81e3ced
Patrick <pconnell89@gmail.com>
2011-09-18 21:59:44 -0400

    new db file

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$mysqli = new mysqli("localhost:/tmp/mysql/skye.sock","ninfa","eG6ZltL7","skye");
+	$mysqli = new mysql("localhost:/tmp/mysql/skye.sock","ninfa","eG6ZltL7","skye");
 	//$db = new mysqli('/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye');// or die('Failed to connect');
 ?>
