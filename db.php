3623d578e44eab18c4816e9e3bf4eb0ccb47ec3a
Patrick <pconnell89@gmail.com>
2011-09-20 17:03:57 -0400

    trying different format for db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$mysqli = mysqli_connect("localhost:/tmp/mysql/skye.sock","ninfa","eG6ZltL7","skye");
-	//$db = new mysqli('/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye');// or die('Failed to connect');
+	//$mysqli = mysqli_connect("localhost:/tmp/mysql/skye.sock","ninfa","eG6ZltL7","skye");
+	$db = new mysqli('/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye');// or die('Failed to connect');
 ?>
