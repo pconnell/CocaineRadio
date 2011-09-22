c489de5ba08ea9f8517d73e81589fa78e3b1d797
Patrick <pconnell89@gmail.com>
2011-09-22 19:53:49 -0400

    entry method

---
+++ sql_query_entry.php
@@ -0,0 +1,15 @@
+<form name = 'insert' method = 'POST' action = 'sql_query_entry.php'>
+	<input type = 'text' id = 'query'/>
+	<input type = 'submit' name = 'enter query'/>
+	<?php
+		include('db.php');
+		if (isset($_POST['query'])){
+			//run query on DB
+			$query = $_POST['query'];
+			$result = mysqli_query($mysqli,$query);
+			if (!$result){
+				echo "there was an error while entering your query. please try again.";
+			}
+		}
+	?>
+</form>
