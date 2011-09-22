21e5ca9026a0dc3d422a4ed891d03c0a75c28012
Patrick <pconnell89@gmail.com>
2011-09-22 19:06:38 -0400

    fixed player error

--- player.php
+++ player.php
@@ -2,10 +2,9 @@
 	$html = file_get_contents('http://www.shoutcast.com/Internet-Radio/CocaineRadio');
 	$pattern = '/id="[0-9]*" title="CocaineRadio"/';
 	$match = array();
-	preg_match($pattern,$html,$match);
-	$id = $match[0];
-	$str = '';
-	if ($id != ''){
+	$str = "<em>We're sorry, but the stream is currently unavailable.</em>";
+	if(preg_match($pattern,$html,$match)){
+		$id = $match[0];
 		$patt = '/"[0-9]*/';
 		$mat = array();
 		preg_match($patt,$id,$mat);
@@ -13,8 +12,6 @@
 		$id = substr($id,1);
 		//echo $id
 		$str = "<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=".$id."&play_status=1' quality='high' bgcolor='#ffffff' width='600' height='104' name='popupPlayer_V19' align='middle' allowScriptAccess='always' allowFullScreen='true' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer' ></embed>" ;
-	}else{
-		$str = "<em>We're sorry, but the stream is currently unavailable.</em>";
 	}
 	echo $str;
 ?>
