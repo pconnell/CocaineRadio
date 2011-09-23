1283ac37414592c33cee9a8b31b4c95276f4b2d2
Patrick <pconnell89@gmail.com>
2011-09-23 18:38:02 -0400

    i think this could do it finally

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -51,17 +51,17 @@ $cfg['Servers'][$i]['controlpass'] = 'pattypass';
 // $cfg['Servers'][$i]['controlpass'] = 'pmapass';
 
 /* Storage database and tables */
-// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-// $cfg['Servers'][$i]['bookmarktable'] = 'pma_bookmark';
-// $cfg['Servers'][$i]['relation'] = 'pma_relation';
-// $cfg['Servers'][$i]['table_info'] = 'pma_table_info';
-// $cfg['Servers'][$i]['table_coords'] = 'pma_table_coords';
-// $cfg['Servers'][$i]['pdf_pages'] = 'pma_pdf_pages';
-// $cfg['Servers'][$i]['column_info'] = 'pma_column_info';
-// $cfg['Servers'][$i]['history'] = 'pma_history';
-// $cfg['Servers'][$i]['tracking'] = 'pma_tracking';
-// $cfg['Servers'][$i]['designer_coords'] = 'pma_designer_coords';
-// $cfg['Servers'][$i]['userconfig'] = 'pma_userconfig';
+ $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+ $cfg['Servers'][$i]['bookmarktable'] = 'pma_bookmark';
+ $cfg['Servers'][$i]['relation'] = 'pma_relation';
+ $cfg['Servers'][$i]['table_info'] = 'pma_table_info';
+ $cfg['Servers'][$i]['table_coords'] = 'pma_table_coords';
+ $cfg['Servers'][$i]['pdf_pages'] = 'pma_pdf_pages';
+ $cfg['Servers'][$i]['column_info'] = 'pma_column_info';
+ $cfg['Servers'][$i]['history'] = 'pma_history';
+ $cfg['Servers'][$i]['tracking'] = 'pma_tracking';
+ $cfg['Servers'][$i]['designer_coords'] = 'pma_designer_coords';
+ $cfg['Servers'][$i]['userconfig'] = 'pma_userconfig';
 /* Contrib / Swekey authentication */
 // $cfg['Servers'][$i]['auth_swekey_config'] = '/etc/swekey-pma.conf';
 
