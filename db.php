e68c5f020de34e71e10db4be41d91fd600ea5736
Patrick <pconnell89@gmail.com>
2011-09-23 19:09:09 -0400

    i hope this works

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$mysqli = mysqli_connect('localhost;unix_socket=/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
+	$mysqli = new mysqli('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
 	//$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
 ?>
