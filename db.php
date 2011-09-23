05681ee7f3c74a69edd8c60a76b186d26fcdb000
Patrick <pconnell89@gmail.com>
2011-09-22 20:03:02 -0400

    geez

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
-	$mysqli = mysqli_connect("/tmp/mysql/deadra.sock","lindsy","XUJQTbOP","deadra");
+	$mysqli = mysqli_connect("localhost;unix_socket=/tmp/mysql/deadra.sock","lindsy","XUJQTbOP","deadra");
 	//$db = new mysqli('/tmp/mysql/skye.sock','ninfa','eG6ZltL7','skye');// or die('Failed to connect');
+	
+	//("localhost;unix_socket=/tmp/mysql/deadra.sock","lindsy","XUJQTbOP","deadra");
 ?>
