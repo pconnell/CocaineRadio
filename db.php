fb16e095af9dbf8401cace473bbbb6f6c9891027
Patrick <pconnell89@gmail.com>
2011-09-22 21:30:11 -0400

    recreated db

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$mysqli = mysqli_connect('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
+	$mysqli = mysqli_connect('coke-radio.pagodabox.com/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
 	//$mysqli = new mysqli('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle') or die('Failed to connect');
 ?>
