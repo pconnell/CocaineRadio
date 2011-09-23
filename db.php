4af3c91604e87f3d5af6dc0366818d05484a57cd
Patrick <pconnell89@gmail.com>
2011-09-22 21:25:16 -0400

    db re-do based upon PG creds.

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
 	//$mysqli = mysqli_connect("localhost;unix_socket=/tmp/mysql/deadra.sock","lindsy","XUJQTbOP","deadra",3307,"deadra.sock");
-	$mysqli = new mysqli('/tmp/mysql/deadra.sock','lindsy','XUJQTbOP','deadra') or die('Failed to connect');
+	$mysqli = new mysqli('localhost:/tmp/mysql/deadra.sock','lindsy','XUJQTbOP','deadra') or die('Failed to connect');
 ?>
