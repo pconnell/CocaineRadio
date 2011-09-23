c6aadf83b43c7e8c704136ee8150804257b76ddf
Patrick <pconnell89@gmail.com>
2011-09-22 21:36:09 -0400

    db.php with new format

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
-	$mysqli = mysqli_connect('http://coke-radio.pagodabox.com/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
+	//$mysqli = mysqli_connect('mysql://coke-radio.pagodabox.com/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
 	//$mysqli = new mysqli('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle') or die('Failed to connect');
+	$mysqli_url = 'mysql://elenore:8dnTIbvF@localhost/belle';
+	$mysqli = mysqli_connect($mysqli_url);
 ?>
