0d541ac8b8f98d2be904bbb088424496574d2140
Patrick <pconnell89@gmail.com>
2011-09-22 21:46:26 -0400

    new method of db connect

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-	//$mysqli = mysqli_connect('mysql://coke-radio.pagodabox.com/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
-	//$mysqli = new mysqli('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle') or die('Failed to connect');
-	$mysqli = mysqli_connect('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
+	//$mysqli = mysqli_connect('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
+	$connection = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
 ?>
