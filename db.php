4f3e2439969d015142b07b4c80d171d7a88e6c75
Patrick <pconnell89@gmail.com>
2011-09-22 21:19:24 -0400

    trying with new mysqli command

--- db.php
+++ db.php
@@ -1,3 +1,4 @@
 <?php
-	$mysqli = mysqli_connect("localhost;unix_socket=/tmp/mysql/deadra.sock","lindsy","XUJQTbOP","deadra",3307,"deadra.sock");
+	//$mysqli = mysqli_connect("localhost;unix_socket=/tmp/mysql/deadra.sock","lindsy","XUJQTbOP","deadra",3307,"deadra.sock");
+	$mysqli = new mysqli('/tmp/mysql/deadra.sock','lindsy','XUJQTbOP','deadra');// or die('Failed to connect');
 ?>
