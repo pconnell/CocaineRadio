a8680758f925fee9d126a00fab40c9c17cc484f7
Patrick <pconnell89@gmail.com>
2011-09-23 20:39:35 -0300

    setting fetch mode for db.php

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
 	$mysqli = new \PDO('mysql:dbname=belle;host=localhost;unix_socket=/tmp/mysql/belle.sock','elenore','4:44mile');
+	$DB->setFetchMode(PDO::FETCH_ASSOC);
 	//$mysqli = new mysqli("localhost;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF','belle');
 	//$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
 ?>
