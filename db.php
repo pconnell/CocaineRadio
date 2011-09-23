58f68c05427a27b8e99d97df898b5715a74ae889
Patrick <pconnell89@gmail.com>
2011-09-22 21:40:18 -0400

    y u no work?

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
 <?php
 	//$mysqli = mysqli_connect('mysql://coke-radio.pagodabox.com/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
 	//$mysqli = new mysqli('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle') or die('Failed to connect');
-	$mysqli_url = 'mysql://elenore:8dnTIbvF@localhost/belle';
-	$mysqli = mysqli_connect($mysqli_url);
+	$mysqli = mysqli_connect('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
 ?>
