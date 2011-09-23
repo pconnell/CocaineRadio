123c78abc9f0ec2b3f06922529288622bbd1cf48
Patrick <pconnell89@gmail.com>
2011-09-23 18:55:00 -0400

    updating .box and db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$mysqli = mysqli_connect('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
-	//$connection = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
+	//$mysqli = mysqli_connect('/tmp/mysql/belle.sock','elenore','8dnTIbvF','belle');
+	$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
 ?>
