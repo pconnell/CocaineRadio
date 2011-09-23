7a647ae4d1935d3492c5070be51d56a51dc2ac4e
Patrick <pconnell89@gmail.com>
2011-09-23 20:32:47 -0300

    ? :D

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new \PDO('mysql:dbname=belle;host=localhost;unix_socket=/tmp/mysql/belle.sock','elenore','8dnTIbvF');
+	$mysqli = new \PDO('mysql:dbname=belle;host=localhost;unix_socket=/tmp/mysql/belle.sock','elenore','4:44mile');
 	//$mysqli = new mysqli("localhost;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF','belle');
 	//$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
 ?>
