9bdbd8cfb91a17e8a8fff55042380e5bafa1dd28
Patrick <pconnell89@gmail.com>
2011-09-23 20:12:26 -0300

    Edited db.php via GitHub

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$mysqli = new mysqli("localhost;unix_socket='/tmp/mysql/belle.sock'",'elenore','8dnTIbvF','belle');
+	$mysqli = new mysqli("localhost;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF','belle');
 	//$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
 ?>
