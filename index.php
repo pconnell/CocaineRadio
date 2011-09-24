9c3ce549d64a812ab61938d4b0a494eee4992923
Patrick <pconnell89@gmail.com>
2011-09-23 20:08:25 -0400

    maybe now...

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
-	/*include('header.php');
-	include('body.php');*/
+	include('header.php');
+	//include('body.php');*/
 	if(isset($_SESSION['username'])){
 		include('body.php');
 	}else{
