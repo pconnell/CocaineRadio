0c63878fffbdce64d54c3ddba0102ce77e97fbef
Patrick <pconnell89@gmail.com>
2011-10-02 15:26:44 -0400

    redo of index

--- index.php
+++ index.php
@@ -1,10 +1,53 @@
 <?php
 	session_start();
-	include('header.php');
-	//include('body.php');*/
+	/*include('header.php');
+	//include('body.php');
 	if(isset($_SESSION['username'])){
 		include('body.php');
 	}else{
 		include('login.php');
+	}*/
+?>
+
+<?php
+	include('header.php');
+?>
+
+<?php
+	if(isset($_SESSION['username'])){
+		echo "<script>ajax_update('/body.php','pageBody')</script>";
+	}else{
+		echo "<script>ajax_update('/login.php','pageBody')</script>"
 	}
 ?>
+
+<body id = 'pageBody'>
+	
+</body>
+
+<footer id = 'foot'>
+	
+</footer>
+
+<script>
+function ajax_update(dataPage,pageObjectID){
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+		{// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp = new XMLHttpRequest();
+		}
+		else
+		{// code for IE6, IE5
+			//write("ActiveXObject");
+		  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.open("GET",''+dataPage,true);
+		xmlhttp.send();
+		xmlhttp.onreadystatechange=function()
+		{
+			if (xmlhttp.readyState==4)
+			{
+				document.getElementById(''+pageObjectID).innerHTML=xmlhttp.responseText;
+			}
+		}
+</script>
