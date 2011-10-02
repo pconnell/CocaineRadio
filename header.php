f2d00604afb9be4168882e3ec3801ac18891a886
Patrick <pconnell89@gmail.com>
2011-10-02 17:39:16 -0400

    y u no?

--- header.php
+++ header.php
@@ -1,12 +1,25 @@
-<?php
-	include('db.php');
-	/*$query = "SELECT * FROM users;";
-	$result = $DB->query($query);
-	if (!$result){
-		echo "<h1>aw...</h1><br/>";
-	}
-	$result->setFetchMode(PDO::FETCH_ASSOC);
-	while ($row = $result->fetch()){
-		echo "<br/><h1>".$row['username']."</h1>";
-	}*/
-?>
+<center>
+		<object id="fmp256" type="application/x-shockwave-flash" data="/flash/minicaster.swf" width="180" height="70">
+		  <param name="movie" value="minicaster.swf" />
+		  <param name="wmode" value="transparent" />
+		   <div class="stirfry">
+		   <h4>Minicaster Radio Playhead</h4>
+		   <p>To listen you must <a href="http://www.macromedia.com/go/getflashplayer/"
+		   title="Click here to install the Flash browser plugin from Macromedia">install Flash Player</a>. Visit
+		   <a href="http://www.draftlight.net/dnex/mp3player/minicaster/" title="Draftlight Networks">Draftlight Networks</a>
+		   for more info.</p></div>
+		</object>		
+		<table id = 'projectData'>
+			<thead>
+				<tr>
+					<th ><a href = "/about.php" target = '_blank'>Info/About</a></th>
+					<th ><a href = "http://pconnell89.umwblogs.org" target = '_blank'>Blog</a></th>
+					<th ><a href = "http://www.twitter.com/CocaineRadio" target = '_blank'>Twitter</a></th>
+					<th ><a href = "http://www.facebook.com/patrick.s.connelly" target = '_blank'>Facebook</a></th>
+					<th><a></a></th>
+				</tr>
+			</thead>
+		</table>
+	</center>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<script type = 'text/javascript' src = 'siteScripts.js'/>
