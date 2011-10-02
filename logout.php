465ef1dd834efd5e9a5a3361d4a96d47081e0370
Patrick <pconnell89@gmail.com>
2011-10-02 17:16:22 -0400

    hurrrrrr

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 	//session_destroy();
 	session_unset($_SESSION['username']);
-	echo "$_SESSION['username']";
+	//echo "$_SESSION['username']";
 	include('index.php');
 	//$login_pg = file_get_contents('/login.php');
 	//echo $login_pg;
