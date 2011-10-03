04bc13786dfc996295fa8ea53754cb983f86d838
Patrick <pconnell89@gmail.com>
2011-10-02 21:31:09 -0400

    fixing login

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 
 <center>
 		<h1>Please log-in to upload, vote, and view play history!</h1>
-		<form id = 'log' method = 'get' action = 'javascript:login();'>
+		<form id = 'log' <!--method = 'get' action = 'javascript:login();'-->>
 		Username:<input type = 'text' id = 'username' style='vertical-align:middle'/>
 		Password:<input type = 'password' id = 'password'/>
-		<input type = 'submit' id = 'submit' value = 'login' onClick = 'login();'/>
+		<input type = 'submit' value = 'login' onClick = 'login();'/>
 	</form>
 </center>
 <script>
