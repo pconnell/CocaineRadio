5bd9fe2d21d801035fdc767a00aa54198ed9d553
Patrick <pconnell89@gmail.com>
2011-10-02 21:46:18 -0400

    ???

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <center>
 		<h1>Please log-in to upload, vote, and view play history!</h1>
-		<form id = 'log' action = ''>
+		<form id = 'log' method = 'get' action = 'javascript:login();'>
 			Username:<input type = 'text' id = 'username' style='vertical-align:middle'/>
 			Password:<input type = 'password' id = 'password'/>
-			<input type = 'submit' value = 'login' action = "login();"/>
+			<input type = 'submit' value = 'login' />
 		</form>
 </center>
