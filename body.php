e40628d86a472e44d9f4c1d61d87529a967ea19a
Patrick <pconnell89@gmail.com>
2011-10-02 22:08:57 -0400

    yeah!

--- body.php
+++ body.php
@@ -1,15 +1,5 @@
 <?php
-	if (!(isset($_SESSION['username']))){
-		echo "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.0 Transitional//EN'>
-<html>
-<head>
-<meta http-equiv='REFRESH' content='0;url=/index.php'></HEAD>
-<BODY>
-No no no...
-</BODY>
-</HTML>";
-	}else{
-		echo "
+echo "
 <center>
 <form method = 'get' action = 'javascript:logout();'>
 	<input type = 'submit' value = 'logout'/>
