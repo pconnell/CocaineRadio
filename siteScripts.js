bbec38edae8da003851fc9fd378caf53d36f91d7
Patrick <pconnell89@gmail.com>
2011-10-02 21:43:08 -0400

    maybe this time

--- siteScripts.js
+++ siteScripts.js
@@ -25,6 +25,33 @@ function logout(){
 	update('/logout.php','pb');
 }
 
+function login(){
+	var form = document.getElementById('log');
+	var username = form.username.value;
+	var password = form.password.value;
+	var space = document.getElementById('pb');
+	//space.innerHTML = "<h1>hello!</h1>"
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+		//document.write("XMLHttp");
+	}
+	else
+	{// code for IE6, IE5
+		//write("ActiveXObject");
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	var queryString = "/login_helper.php?username="+username+"&password="+password;
+	xmlhttp.open("GET",queryString,true);
+	xmlhttp.send(null);
+	
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == 4){
+			space.innerHTML = xmlhttp.responseText;
+		}
+	}
+}
 function pls(){
 	var xmlhttp;
 	if (window.XMLHttpRequest)
