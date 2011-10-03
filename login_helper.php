3b1b775e98ddd1ff5394ad97be7b19ddb8025de8
Patrick <pconnell89@gmail.com>
2011-10-02 22:13:39 -0400

    fixing login thingy

--- login_helper.php
+++ login_helper.php
@@ -10,7 +10,7 @@
 	$result->setFetchMode(PDO::FETCH_ASSOC);
 	while($row = $result->fetch()){
 		if ($row['id'] != ''){
-			//echo "yay!<br/>";
+			echo "yay!<br/>";
 			$_SESSION['username'] = $row['username'];
 			$_SESSION['userID'] = $row['id'];
 			$_SESSION['email'] = $row['email'];
@@ -19,7 +19,7 @@
 		}else{
 			echo "<center><h2>incorrect login info, please try again</h2></center>";
 			//include('header.php');
-			include('login_form.php');
+			include('login.php');
 		}
 	}
 ?>
