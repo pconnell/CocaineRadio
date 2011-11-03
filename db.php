030ac9c904ea0c9bc69b197768cae0c3bc885c9a
Patrick <pconnell89@gmail.com>
2011-11-03 12:05:53 -0400

    trying mysqli for db connection

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-	//$DB = new \PDO('mysql:dbname=db1;host=tunnel.pagodabox.com;port=3306;'hanh','9S3X4nvm');
+	$DB = new \PDO('mysql:dbname=db1;host=tunnel.pagodabox.com;port=3306;'hanh','9S3X4nvm');
 	//$DB->setFetchMode(PDO::FETCH_ASSOC);
 	//$mysqli = new mysqli("localhost;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF','belle');
 	//$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
-          $DB = new mysqli('tunnel.pagodabox.com:3306','hanh','9S3X4nvm','radio_data');
+         // $DB = new mysqli('tunnel.pagodabox.com:3306','hanh','9S3X4nvm','radio_data');
 ?>
