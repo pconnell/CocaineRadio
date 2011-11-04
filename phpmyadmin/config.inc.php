0dde1c60d51836f21e91e9d048e7f001505dd51e
Patrick <pconnell89@gmail.com>
2011-11-03 23:50:08 -0400

    fixed

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -1,20 +1,9 @@
 <?php
-/* vim: set expandtab sw=4 ts=4 sts=4: */
-/**
- * phpMyAdmin sample configuration, you can use it as base for
- * manual configuration. For easier setup you can use setup/
- *
- * All directives are explained in Documentation.html and on phpMyAdmin
- * wiki <http://wiki.phpmyadmin.net>.
- *
- * @package phpMyAdmin
- */
-
 /*
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = 'blah'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'blargh'; /* YOU SHOULD CHANGE THIS FOR A MORE SECURE COOKIE AUTH! */
 
 /*
  * Servers configuration
@@ -25,45 +14,36 @@ $i = 0;
  * First server
  */
 $i++;
-/* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
+
+/* Authentication type and info */
+$cfg['Servers'][$i]['auth_type']            = 'config';
+$cfg['Servers'][$i]['user']                 = 'hanh';
+$cfg['Servers'][$i]['password']             = '9S3X4nvm';
+$cfg['Servers'][$i]['AllowNoPassword']      = false;
+
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
-//$cfg['Servers'][$i]['connect_type'] = 'tcp';
-$cfg['Servers'][$i]['port'] = '';
-$cfg['Servers'][$i]['user'] = 'elenore';
-$cfg['Servers'][$i]['password'] = '8dnTIbvF';
-$cfg['Servers'][$i]['connect_type'] = 'socket';
-$cfg['Servers'][$i]['socket'] = '/tmp/mysql/belle.sock';
-$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['host']             = 'tunnel.pagodabox.com';
+$cfg['Servers'][$i]['port']				= '3307';
+$cfg['Servers'][$i]['connect_type']     = 'tcp';
+$cfg['Servers'][$i]['compress']         = false;
+
 /* Select mysqli if your server has it */
 $cfg['Servers'][$i]['extension'] = 'mysqli';
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
 
-/*
- * phpMyAdmin configuration storage settings.
- */
-$cfg['Servers'][$i]['controluser'] = 'elenore';
-$cfg['Servers'][$i]['controlpass'] = '8dnTIbvF';
- 
-/* User used to manipulate with storage */
-// $cfg['Servers'][$i]['controluser'] = 'pma';
-// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
+/* User for advanced features */
+$cfg['Servers'][$i]['controluser'] = 'hanh';
+$cfg['Servers'][$i]['controlpass'] = '9S3X4nvm';
 
-/* Storage database and tables */
- $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
- $cfg['Servers'][$i]['bookmarktable'] = 'pma_bookmark';
- $cfg['Servers'][$i]['relation'] = 'pma_relation';
- $cfg['Servers'][$i]['table_info'] = 'pma_table_info';
- $cfg['Servers'][$i]['table_coords'] = 'pma_table_coords';
- $cfg['Servers'][$i]['pdf_pages'] = 'pma_pdf_pages';
- $cfg['Servers'][$i]['column_info'] = 'pma_column_info';
- $cfg['Servers'][$i]['history'] = 'pma_history';
- $cfg['Servers'][$i]['tracking'] = 'pma_tracking';
- $cfg['Servers'][$i]['designer_coords'] = 'pma_designer_coords';
- $cfg['Servers'][$i]['userconfig'] = 'pma_userconfig';
-/* Contrib / Swekey authentication */
-// $cfg['Servers'][$i]['auth_swekey_config'] = '/etc/swekey-pma.conf';
+/* Advanced phpMyAdmin features */
+$cfg['Servers'][$i]['pmadb']            = 'phpmyadmin';
+$cfg['Servers'][$i]['bookmarktable']    = 'pma_bookmark';
+$cfg['Servers'][$i]['relation']         = 'pma_relation';
+$cfg['Servers'][$i]['table_info']       = 'pma_table_info';
+$cfg['Servers'][$i]['table_coords']     = 'pma_table_coords';
+$cfg['Servers'][$i]['pdf_pages']        = 'pma_pdf_pages';
+$cfg['Servers'][$i]['column_info']      = 'pma_column_info';
+$cfg['Servers'][$i]['history']          = 'pma_history';
+$cfg['Servers'][$i]['designer_coords']  = 'pma_designer_coords';
 
 /*
  * End of servers configuration
@@ -72,79 +52,15 @@ $cfg['Servers'][$i]['controlpass'] = '8dnTIbvF';
 /*
  * Directories for saving/loading files from server
  */
-$cfg['UploadDir'] = '';
-$cfg['SaveDir'] = '';
+$cfg['UploadDir']   = '';
+$cfg['SaveDir']     = '';
 
-/**
- * Defines whether a user should be displayed a "show all (records)"
- * button in browse mode or not.
- * default = false
- */
-//$cfg['ShowAll'] = true;
+$cfg['AllowAnywhereRecoding']       = true;
+$cfg['DefaultCharset']              = 'utf-8';
+$cfg['DefaultLang']                 = 'en-utf-8';
+$cfg['DefaultConnectionCollation']  = 'utf8_general_ci';
 
-/**
- * Number of rows displayed when browsing a result set. If the result
- * set contains more rows, "Previous" and "Next".
- * default = 30
- */
-//$cfg['MaxRows'] = 50;
+$cfg['MySQLManualBase'] = 'http://dev.mysql.com/doc/refman/5.1/en';
+$cfg['MySQLManualType'] = 'searchable';
 
-/**
- * Use graphically less intense menu tabs
- * default = false
- */
-//$cfg['LightTabs'] = true;
-
-/**
- * disallow editing of binary fields
- * valid values are:
- *   false  allow editing
- *   'blob' allow editing except for BLOB fields
- *   'all'  disallow editing
- * default = blob
- */
-//$cfg['ProtectBinary'] = 'false';
-
-/**
- * Default language to use, if not browser-defined or user-defined
- * (you find all languages in the locale folder)
- * uncomment the desired line:
- * default = 'en'
- */
-//$cfg['DefaultLang'] = 'en';
-//$cfg['DefaultLang'] = 'de';
-
-/**
- * default display direction (horizontal|vertical|horizontalflipped)
- */
-//$cfg['DefaultDisplay'] = 'vertical';
-
-
-/**
- * How many columns should be used for table display of a database?
- * (a value larger than 1 results in some information being hidden)
- * default = 1
- */
-//$cfg['PropertiesNumColumns'] = 2;
-
-/**
- * Set to true if you want DB-based query history.If false, this utilizes
- * JS-routines to display query history (lost by window close)
- *
- * This requires configuration storage enabled, see above.
- * default = false
- */
-//$cfg['QueryHistoryDB'] = true;
-
-/**
- * When using DB-based query history, how many entries should be kept?
- *
- * default = 25
- */
-//$cfg['QueryHistoryMax'] = 100;
-
-/*
- * You can find more configuration options in Documentation.html
- * or here: http://wiki.phpmyadmin.net/pma/Config
- */
 ?>
