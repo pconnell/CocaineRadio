6d44b0dd7cd22732db36170aa1818aabdbc50f0c
Patrick <pconnell89@gmail.com>
2011-11-03 21:21:43 -0400

    fixed login_helper.php for mysqli

--- login_helper.php
+++ login_helper.php
@@ -2,28 +2,30 @@
 	session_start();
 ?>
 <?php
+<?php
+	session_start();
+?>
+<?php
 	include('db.php');
 	$pw = $_GET['password'];
 	$user = $_GET['username'];
 	$query = "SELECT * FROM users WHERE username = '$user' AND pass = SHA('$pw');";
-	//$prep = $DB->prepare($query);
-	//$result = $prep->execute();
-	$result = $DB->query($query);
-	$result->setFetchMode(PDO::FETCH_ASSOC);
-	$val = false;
-	while($row = $result->fetch()){
+	//$query = "SELECT * FROM users WHERE username = '$user';";
+	//echo $query;
+	$res = mysqli_query($db,$query);
+	while($row = mysqli_fetch_array($res)){
 		if ($row['id'] != ''){
-			$val = true;
+			//echo "yay!<br/>";
 			$_SESSION['username'] = $row['username'];
-			$_SESSION['userID'] = $row['id'];
+			$_SESSION['userID'] = $row['userID'];
 			$_SESSION['email'] = $row['email'];
 			//include('header.php');
 			include('body.php');
+		}else{
+			echo "<center><h2>incorrect login info, please try again</h2></center>";
+			//include('header.php');
+			include('login_form.php');
 		}
 	}
-	if(!$val){
-		echo "<center><h2>incorrect login info, please try again</h2></center>";
-			//include('header.php');
-			include('login.php');
-	}
+?>
 ?>
