b8676340374dee8be11e7ed7eed6fc4057a94a91
Patrick <pconnell89@gmail.com>
2011-11-03 23:38:00 -0400

    attempting to fix phpmyadmin connection

--- db.php
+++ db.php
@@ -1,7 +1,3 @@
 <?php
-	//$DB = new \PDO('mysql:dbname=db1;host=tunnel.pagodabox.com;port=3306','hanh','9S3X4nvm');
-	//$DB->setFetchMode(PDO::FETCH_ASSOC);
-	//$mysqli = new mysqli("localhost;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF','belle');
-	//$mysqli = new PDO("mysql:host=localhost;dbname=belle;unix_socket=/tmp/mysql/belle.sock",'elenore','8dnTIbvF');
     $DB = mysqli_connect('tunnel.pagodabox.com','hanh','9S3X4nvm','radio_data',3306);
 ?>
